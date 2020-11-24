bd565da7763f8a9edda3e9ea3e55901cd1ff5df0
Sahith Reddy Mallem <helloswapna@gmail.com>
2020-11-23 19:22:26 -0600

    Hello

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    
     bird = new Bird(100,100);
 
 }
@@ -61,4 +61,6 @@ function draw(){
 
     bird.display();
     platform.display();
-}
+// Hello
+//My name is Sahith
+//I am your creator
